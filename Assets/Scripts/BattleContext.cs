9426aa3d4b4262973ac8cd33f55ae947a1b19d32
Jinnnyjin <jinnnny.jin@gmail.com>
2026-08-25 14:44:19 +0900

    [Add] BattleContext 클래스 추가
    도장 인터페이스가 공통으로 참조할 전투 상황 정보, 전투 로직 설계 시 필요한 필드 채워질 예정(현재는 빈 클래스로 우선 생성)

---
+++ Assets/Scripts/BattleContext.cs
@@ -0,0 +1,12 @@
+
+// TODO : 전투 로직 설계시 필요한 필드 채워나가기
+
+/// <summary>
+/// 도장 인터페이스 (IAttackModifier, ITurnEndTrigger, IPassiveModifier, IBlockInteraction)의
+/// 함수가 실행될 때 전달되는 정보
+/// 여러 매개변수를 나열하는 대신 하나로 묶어 전달하기 위해 사용
+/// </summary>
+public class BattleContext
+{
+
+}
